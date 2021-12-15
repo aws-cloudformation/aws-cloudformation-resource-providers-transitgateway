628c001e51200e75d2aa9942e12f166570b12628
Suhani Ladani <lsuhani@amazon.com>
2021-12-15 11:13:55 -0500

    fix the leaked resources canary issue

--- aws-ec2-transitgatewaymulticastgroupmember/src/main/java/software/amazon/ec2/transitgatewaymulticastgroupmember/workflow/create/CreatePrecheck.java
+++ aws-ec2-transitgatewaymulticastgroupmember/src/main/java/software/amazon/ec2/transitgatewaymulticastgroupmember/workflow/create/CreatePrecheck.java
@@ -51,7 +51,13 @@ public class CreatePrecheck {
         } else {
             ResourceModel current = this.makeRequest();
             if(current != null) {
-                return this.failedRequest();
+                CfnResourceConflictException exception =  new CfnResourceConflictException(ResourceModel.TYPE_NAME, model.getPrimaryIdentifier().toString().replace("/properties/", ""), "Cannot be modified by ACTION: CREATE. A resource with the primary identifier already exists");
+                return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                        .resourceModel(model)
+                        .status(OperationStatus.FAILED)
+                        .errorCode(HandlerErrorCode.AlreadyExists)
+                        .message(exception.getMessage())
+                        .build();
             } else {
                 return this.progress;
             }
