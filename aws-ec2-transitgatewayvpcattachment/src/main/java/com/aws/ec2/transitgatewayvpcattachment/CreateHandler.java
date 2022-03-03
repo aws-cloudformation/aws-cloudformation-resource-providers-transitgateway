80f9fcf8a9642aa58ddca4bda7824b486b632b8a
Sandeep Bellary <sbellar@amazon.com>
2022-03-03 16:39:46 -0500

    Add CreatePreCheck for Vpc Attachment

--- aws-ec2-transitgatewayvpcattachment/src/main/java/com/aws/ec2/transitgatewayvpcattachment/CreateHandler.java
+++ aws-ec2-transitgatewayvpcattachment/src/main/java/com/aws/ec2/transitgatewayvpcattachment/CreateHandler.java
@@ -1,6 +1,7 @@
 package com.aws.ec2.transitgatewayvpcattachment;
 
 import com.aws.ec2.transitgatewayvpcattachment.workflow.create.Create;
+import com.aws.ec2.transitgatewayvpcattachment.workflow.create.CreatePreCheck;
 import com.aws.ec2.transitgatewayvpcattachment.workflow.read.Read;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
@@ -17,6 +18,7 @@ public class CreateHandler extends BaseHandlerStd {
 
         if(callbackContext.getAttempts() == 1 &&  (request.getDesiredResourceState().getId() != null)) throw new CfnInvalidRequestException("Attempting to set a ReadOnly Property.");
         return ProgressEvent.progress(request.getDesiredResourceState(), callbackContext)
+            .then(new CreatePreCheck(proxy, request, callbackContext, proxyClient, logger):: run)
             .then(new Create(proxy, request, callbackContext, proxyClient, logger)::run)
             .then(new Read(proxy, request, callbackContext, proxyClient, logger)::run);
     }
