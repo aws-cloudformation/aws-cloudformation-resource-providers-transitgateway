8738ad18ed1690b8f05fd00feb472aee27fdf4a8
Shweta.Singh <snshweta@amazon.com>
2023-03-31 11:05:44 -0400

    AWS::EC2::TransitGatewayRouteTable updated as per Readiness Review Doc

--- aws-ec2-transitgatewayroutetable/src/main/java/software/amazon/ec2/transitgatewayroutetable/DeleteHandler.java
+++ aws-ec2-transitgatewayroutetable/src/main/java/software/amazon/ec2/transitgatewayroutetable/DeleteHandler.java
@@ -42,18 +42,18 @@ public class DeleteHandler extends BaseHandlerStd {
             return ProgressEvent.failed(resourceModel, callbackContext, HandlerErrorCode.InvalidRequest, "Transit Gateway Route Table ID cannot be empty");
         }
         return ProgressEvent.progress(resourceModel, callbackContext)
-            .then(progress -> new ReadHandler().handleRequest(proxy, request, callbackContext, proxyClient, logger))
-            .onSuccess(progress ->
-            proxy.initiate("AWS-EC2-TransitGatewayRouteTable::Delete", proxyClient, resourceModel, callbackContext)
-            .translateToServiceRequest(Translator::translateToDeleteRequest)
-                    .makeServiceCall((awsRequest, client) -> deleteTransitGatewayRouteTable(proxyClient, logger, awsRequest, resourceModel))
-            .stabilize((awsRequest, awsResponse, client, model, context) ->
-               stabilizeDelete(proxyClient, model))
-            .handleError((awsRequest, exception, client, model, context) -> handleError(awsRequest, exception, client, model, context, logger))
-                    .progress()
+                .then(progress -> new ReadHandler().handleRequest(proxy, request, callbackContext, proxyClient, logger))
+                .onSuccess(progress ->
+                        proxy.initiate("AWS-EC2-TransitGatewayRouteTable::Delete", proxyClient, resourceModel, callbackContext)
+                                .translateToServiceRequest(Translator::translateToDeleteRequest)
+                                .makeServiceCall((awsRequest, client) -> deleteTransitGatewayRouteTable(proxyClient, logger, awsRequest, resourceModel))
+                                .stabilize((awsRequest, awsResponse, client, model, context) ->
+                                        stabilizeDelete(proxyClient, model))
+                                .handleError((awsRequest, exception, client, model, context) -> handleError(awsRequest, exception, client, model, context, logger))
+                                .progress()
                 )
-            .then(progress -> ProgressEvent.defaultSuccessHandler(null));
-}
+                .then(progress -> ProgressEvent.defaultSuccessHandler(null));
+    }
 
     /**
      * Delete Transit Gateway Route Table Call Chain. This method deletes a Transit Gateway Route Table and also waits for Stabilization
@@ -72,21 +72,22 @@ public class DeleteHandler extends BaseHandlerStd {
             DeleteTransitGatewayRouteTableResponse response = null;
             final String state = describeTransitGatewayRouteTablesResponse.transitGatewayRouteTables().get(0).stateAsString();
 
-            if ("pending".equals(state)
-                    || disassociateRouteTable(model.getTransitGatewayRouteTableId(), proxyClient)
+            if (state!= null && ("pending".equals(state)
+                    || disassociateRouteTable(model.getTransitGatewayRouteTableId(), proxyClient))
             ) {
                 logger.log(String.format("Transit Gateway Route Table %s did not stabilize", model.getTransitGatewayRouteTableId()));
             }
-            if (!"deleting".equals(state)
-                    && !"deleted".equals(state)
+            if (state!= null && (!"deleting".equals(state)
+                    && !"deleted".equals(state))
             ) {
                 response = proxyClient.injectCredentialsAndInvokeV2(awsRequest,
                         proxyClient.client()::deleteTransitGatewayRouteTable);
             }
             return  response;
         } catch(Exception e) {
-            if (!getErrorCode(e).equals(BaseHandlerStd.INCORRECT_STATE) &&
-                    !getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_MALFORMED) &&
+            if(getErrorCode(e).equals(BaseHandlerStd.INCORRECT_STATE))
+                logger.log(getErrorCode(e));
+            if (!getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_MALFORMED) &&
                     !getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_NOT_FOUND)
             ) {
                 logger.log(getErrorCode(e));
@@ -98,54 +99,54 @@ public class DeleteHandler extends BaseHandlerStd {
 
 
 
-        /**
-         * Method to disassociate the specified Transit Gateway Route Table ID
-         * @param transitGatewayRouteTableId
-         * @param proxyClient
-         * @return boolean value indicating the status of transit gateway route table attachment
-         */
+    /**
+     * Method to disassociate the specified Transit Gateway Route Table ID
+     * @param transitGatewayRouteTableId
+     * @param proxyClient
+     * @return boolean value indicating the status of transit gateway route table attachment
+     */
 
     private Boolean disassociateRouteTable(final String transitGatewayRouteTableId, final ProxyClient<Ec2Client> proxyClient) {
-          try {
-              GetTransitGatewayRouteTableAssociationsRequest getTransitGatewayRouteTableAssociationsRequest = GetTransitGatewayRouteTableAssociationsRequest.builder()
-                      .transitGatewayRouteTableId(transitGatewayRouteTableId).build();
+        try {
+            GetTransitGatewayRouteTableAssociationsRequest getTransitGatewayRouteTableAssociationsRequest = GetTransitGatewayRouteTableAssociationsRequest.builder()
+                    .transitGatewayRouteTableId(transitGatewayRouteTableId).build();
 
-              GetTransitGatewayRouteTableAssociationsResponse response = proxyClient.injectCredentialsAndInvokeV2(
-                      getTransitGatewayRouteTableAssociationsRequest,
-                      proxyClient.client()::getTransitGatewayRouteTableAssociations);
-              List<TransitGatewayRouteTableAssociation> transitGatewayRouteTableAssociations = null;
-              DisassociateTransitGatewayRouteTableRequest disassociateTransitGatewayRouteTableRequest = DisassociateTransitGatewayRouteTableRequest.builder()
-                      .build();
+            GetTransitGatewayRouteTableAssociationsResponse response = proxyClient.injectCredentialsAndInvokeV2(
+                    getTransitGatewayRouteTableAssociationsRequest,
+                    proxyClient.client()::getTransitGatewayRouteTableAssociations);
+            List<TransitGatewayRouteTableAssociation> transitGatewayRouteTableAssociations = null;
+            DisassociateTransitGatewayRouteTableRequest disassociateTransitGatewayRouteTableRequest = DisassociateTransitGatewayRouteTableRequest.builder()
+                    .build();
 
-              if (response != null && response.hasAssociations()) {
-                  transitGatewayRouteTableAssociations = response.associations();
-              }
+            if (response != null && response.hasAssociations()) {
+                transitGatewayRouteTableAssociations = response.associations();
+            }
 
-              if (transitGatewayRouteTableAssociations != null && !transitGatewayRouteTableAssociations.isEmpty()) {
-                  for (TransitGatewayRouteTableAssociation association : transitGatewayRouteTableAssociations) {
+            if (transitGatewayRouteTableAssociations != null && !transitGatewayRouteTableAssociations.isEmpty()) {
+                for (TransitGatewayRouteTableAssociation association : transitGatewayRouteTableAssociations) {
 
-                      if (association.stateAsString().equals("associated")) {
+                    if (association.stateAsString().equals("associated")) {
 
-                          DisassociateTransitGatewayRouteTableRequest.builder()
-                                  .transitGatewayAttachmentId(association.transitGatewayAttachmentId())
-                                  .transitGatewayRouteTableId(transitGatewayRouteTableId).
-                                  build();
+                        DisassociateTransitGatewayRouteTableRequest.builder()
+                                .transitGatewayAttachmentId(association.transitGatewayAttachmentId())
+                                .transitGatewayRouteTableId(transitGatewayRouteTableId).
+                                build();
 
-                          proxyClient.client()
-                                  .disassociateTransitGatewayRouteTable(disassociateTransitGatewayRouteTableRequest);
-                      }
-                  }
-                  return true;
-              }
+                        proxyClient.client()
+                                .disassociateTransitGatewayRouteTable(disassociateTransitGatewayRouteTableRequest);
+                    }
+                }
+                return true;
+            }
 
-          }
-          catch (Exception e) {
-              if (!getErrorCode(e).equals(BaseHandlerStd.INVALID_ASSOCIATION_NOT_FOUND) &&
-                      !getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_NOT_FOUND) &&
-                      !getErrorCode(e).equals(BaseHandlerStd.INVALID_TRANSIT_GATEWAY_ATTACHMENT_ID_NOT_FOUND)) {
-                         return false;
-              }
-          }
+        }
+        catch (Exception e) {
+            if (!getErrorCode(e).equals(BaseHandlerStd.INVALID_ASSOCIATION_NOT_FOUND) &&
+                    !getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_NOT_FOUND) &&
+                    !getErrorCode(e).equals(BaseHandlerStd.INVALID_TRANSIT_GATEWAY_ATTACHMENT_ID_NOT_FOUND)) {
+                return false;
+            }
+        }
         return false;
     }
 
@@ -156,7 +157,7 @@ public class DeleteHandler extends BaseHandlerStd {
      * @param proxyClient
      * @param model
      * @return boolean value indicating the status of the stabilization
-    */
+     */
     protected  Boolean stabilizeDelete(final ProxyClient<Ec2Client> proxyClient, final ResourceModel model) {
         try {
             DescribeTransitGatewayRouteTablesRequest describeTransitGatewayRouteTablesRequest = DescribeTransitGatewayRouteTablesRequest.builder().transitGatewayRouteTableIds(model.getTransitGatewayRouteTableId()).build();
@@ -170,13 +171,13 @@ public class DeleteHandler extends BaseHandlerStd {
             }
             return false;
         } catch (Exception e)
-            {
-              logger.log(String.format(" Stabilization Error Code %s",getErrorCode(e)));
-             if (getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_NOT_FOUND) || getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_NOT_FOUND)
-             ) {
+        {
+            logger.log(String.format(" Stabilization Error Code %s",getErrorCode(e)));
+            if (getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_TABLE_ID_NOT_FOUND) || getErrorCode(e).equals(BaseHandlerStd.INVALID_ROUTE_NOT_FOUND)
+            ) {
                 return true;
-             }
-             return false;
+            }
+            return false;
         }
     }
 }
